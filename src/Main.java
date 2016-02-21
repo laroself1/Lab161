cfa9e76e8ce4ca5c501907cbe23bc43d6808a051
laroself1 <laroself1@gmail.com>
2016-02-21 02:26:31 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<Integer>();
+        int i;
+          for (i=1; i<=30; i++){
+            if (i%2==0){
+                myList.add(i);}}
+        System.out.print(myList);}}
